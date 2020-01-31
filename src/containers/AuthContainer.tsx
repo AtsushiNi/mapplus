0b3b9e0e36e27e23527e3e37e7b48dce1c56f82d
AtsushiNi <atsushi9731@gmail.com>
2020-02-01 00:26:01 +0900

    create app-tsx

---
+++ src/containers/AuthContainer.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+class AuthContainer extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>
+          AuthContainer
+        </Text>
+      </View>
+        )
+  }
+}
+
+export default AuthContainer

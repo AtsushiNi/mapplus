8c46c9b395f76e0c98e115a9e9bfef278b780350
AtsushiNi <atsushi9731@gmail.com>
2020-02-15 22:51:22 +0900

    navigationはredux別管理だったぽい

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,9 @@
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
-import { NavigationState } from 'react-navigation'
-import { createNavigationReducer } from 'react-navigation-redux-helpers'
 import thunk from 'redux-thunk'
-import Navigation from './navigations'
 import UserModuleState from './modules/UserModule'
 import { userModule } from './modules'
 
 export interface ReduxState {
-  navigation?: NavigationState
   userModule?: UserModuleState
 }
 
@@ -15,7 +11,6 @@ const composeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || 
 
 const store = createStore(
   combineReducers({
-    navigationModule: createNavigationReducer(Navigation),
     userModule: userModule.reduce
   }),
   composeEnhancer(applyMiddleware(thunk))
